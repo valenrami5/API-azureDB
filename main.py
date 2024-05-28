39ed18aa8ffafbe2856cc11365b8d909b985ec40
valenrami5 <valentina.ramirezz@udea.edu.co>
2024-05-27 19:05:06 -0500

    adding main

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import uvicorn
+import csv
 from app.infrastructure.api import app
+from app.infrastructure import azure_db_connector
+from pydantic import field_validator
+from typing import List, Dict
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from pydantic import field_validator, model_validator
+from typing import List, Dict
+import pandas as pd
+from app.domain.models.jobs import Jobs
 
 if __name__ == "__main__":
+
     uvicorn.run(app, host="0.0.0.0", port=8000)
